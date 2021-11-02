c97a06bf76adae80ea0d2c2782f0e65b528e3194
Jeonkermit <92032207+Jeonkermit@users.noreply.github.com>
2021-11-02 19:51:45 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val number = findViewById<TextView>(R.id.number)
+        val intent = Intent(this, MainActivity2::class.java)
+
+        val toast = findViewById<Button>(R.id.mybutton1)
+        toast.setOnClickListener {
+            Toast.makeText(this, "toast 메세지", Toast.LENGTH_LONG).show();
+        }
+
+        val count = findViewById<Button>(R.id.mybutton2)
+        var num = 0
+        count.setOnClickListener {
+            num += 1
+            number.setText(""+num)
+            intent.putExtra("num", num)
+        }
+
+        val random = findViewById<Button>(R.id.mybutton3)
+        random.setOnClickListener {
+            intent.putExtra("num", num)
+            startActivity(intent)
+        }
     }
 }
